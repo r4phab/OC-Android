19d09b56e49a51f0427b8f8b3d6e7be38c1a4986
JFV34 <jfr.vincler@live.fr>
2018-10-04 10:29:31 +0200

    Adding the 5 Layouts

---
+++ exercises/constraintLayouts/Layout/app/src/main/java/com/example/jfrvi/layout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jfrvi.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Layout1);
+    }
+}

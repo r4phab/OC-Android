f1939032faa0ace0e9f264c5ab4a15db44c37ff1
JFV34 <jfr.vincler@live.fr>
2018-11-05 17:10:29 +0100

    Exercices OOP

---
+++ ExercisesOOP/app/src/main/java/com/vincler/jf/exercisesoop/CarBrand.java
@@ -0,0 +1,11 @@
+package com.vincler.jf.exercisesoop;
+
+public class CarBrand {
+    String name;
+    String address;
+
+    CarBrand(String pName, String pAddress) {
+        name = pName;
+        address = pAddress;
+    }
+}
